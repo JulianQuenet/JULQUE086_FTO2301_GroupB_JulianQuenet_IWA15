6eb9a9d3f07f25b3123d0a38a20fa7394e2831e6
JulianQuenet <julianquenet8@gmail.com>
2023-04-24 11:22:28 +0200

    Changed the code completely, to find largest num

--- scripts.js
+++ scripts.js
@@ -6,54 +6,49 @@ const data = {
   ],
 };
 
-// Should log out the following: [10, 12, 14, 5, 7, 13, 11, 15, 4, 8, 6, 2, 1, 3, 9]
-// 10,12,14 ('second')
-// 5,7,13,11,15 ('first')
-// 4,8,6,2 ('second')
-// 1,3,9 ('third')
 
 // Only edit below
-let count = 1;
 
-const [, first] = data.lists[0];
-const [, second] = data.lists[1];
-const [, third] = data.lists[2];
+const [, first] = data.lists[0]
+const [, second] = data.lists[1]
+const [, third] = data.lists[2]  
 
-const result = [];
+const result = [] ;
 
 const extractBiggest = () => {
-  if (count <= 3 || (count > 8 && count <= 12)) {
-    let number = second.pop();
-    count++;
-    return number;
-  } else if (count <= 8) {
-    let number = first.pop();
-    count++;
-    return number;
-  } else {
-    let number = third.pop();
-    return number;
+  const big1 = first.length > 0 ? first[first.length-1] : -Infinity;
+  const big2 = second.length > 0 ? second[second.length-1] : -Infinity;
+  const big3 = third.length > 0 ? third[third.length-1] : -Infinity;
+  let arr = [big1,big2,big3];
+  let number = Math.max(...arr);
+  if (number === big1) {
+    first.pop();
+  } else if (number === big2) {
+    second.pop();
+  } else if (number === big3) {
+    third.pop(); 
   }
+  return number;
 };
 
 // Only edit above
 
-result.push(extractBiggest()); // Second
-result.push(extractBiggest()); // Second
-result.push(extractBiggest()); // Second
-result.push(extractBiggest()); // First
-result.push(extractBiggest()); // First
+result.push(extractBiggest()); 
+result.push(extractBiggest()); 
+result.push(extractBiggest()); 
+result.push(extractBiggest()); 
+result.push(extractBiggest()); 
 
-result.push(extractBiggest()); // First
-result.push(extractBiggest()); // First
-result.push(extractBiggest()); // First
-result.push(extractBiggest()); // Second
-result.push(extractBiggest()); // Second
+result.push(extractBiggest()); 
+result.push(extractBiggest()); 
+result.push(extractBiggest()); 
+result.push(extractBiggest()); 
+result.push(extractBiggest()); 
 
-result.push(extractBiggest()); // Second
-result.push(extractBiggest()); // Second
-result.push(extractBiggest()); // Third
-result.push(extractBiggest()); // Third
-result.push(extractBiggest()); // Third
+result.push(extractBiggest()); 
+result.push(extractBiggest()); 
+result.push(extractBiggest()); 
+result.push(extractBiggest()); 
+result.push(extractBiggest()); 
 
 console.log(result);
